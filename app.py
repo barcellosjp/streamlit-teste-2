2a7d0df23de4e781cc7edfde5ef914f62b5885e2
barcellosjp <jp_barcellos@hotmail.com>
2024-08-08 21:46:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,22 @@
-import pandas as pd
+import streamlit as st
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-import streamlit as st
 
 # Configuração do Streamlit Secrets
 secrets = st.secrets["gcp"]
 
-# ID da planilha pública
-id_planilha = '1UvAo5LKW2bZf7tKoxoG-BNXwee6KUIf7Iq70u9zpvjI'
-# URL da planilha pública
-url = f"https://docs.google.com/spreadsheets/d/{id_planilha}/export?format=csv"
-
-# Defina o escopo e credenciais usando secrets
+# Definindo o escopo e autenticando
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_dict(
-    {
-        "type": "service_account",
-        "project_id": st.secrets["google"]["project_id"],
-        "private_key_id": st.secrets["google"]["private_key_id"],
-        "private_key": st.secrets["google"]["private_key"].replace('\\n', '\n'),
-        "client_email": st.secrets["google"]["client_email"],
-        "client_id": st.secrets["google"]["client_id"],
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": st.secrets["google"]["client_x509_cert_url"]
-    },
-    scope
-)
-
+creds = ServiceAccountCredentials.from_json_keyfile_dict(secrets, scope)
 client = gspread.authorize(creds)
 
-# Abra a planilha usando o ID ou URL
-sheet = client.open_by_url(url).sheet1
+# Acessando a planilha
+spreadsheet_url = "https://docs.google.com/spreadsheets/d/1UvAo5LKW2bZf7tKoxoG-BNXwee6KUIf7Iq70u9zpvjI/export?format=csv"
+sheet = client.open_by_url(spreadsheet_url).sheet1
+
+# Lendo os dados da planilha
 data = sheet.get_all_records()
 
-# Exiba os dados usando Streamlit
+# Exibindo os dados no Streamlit
+st.title('Dashboard do Google Sheets')
 st.write(data)
